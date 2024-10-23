2322b7583f0c2316e284556db938540d57161048
reacyan <1209614944@qq.com>
2024-10-23 19:16:33 +0800

    Unequip items

--- Assets/Scripts/UI/UI_ItemSlot.cs
+++ Assets/Scripts/UI/UI_ItemSlot.cs
@@ -41,11 +41,14 @@ public class UI_ItemSlot : MonoBehaviour, IPointerDownHandler
         itemText.text = "";
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    public virtual void OnPointerDown(PointerEventData eventData)
     {
-        if (item.data.itemType == ItemType.Equipment)
+        if (item != null)
         {
-            Inventory.instance.EquipItem(item.data);
+            if (item.data.itemType == ItemType.Equipment)
+            {
+                Inventory.instance.EquipItem(item.data);
+            }
         }
     }
 }
